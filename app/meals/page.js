d9b76215292956a31420f3eb543c0978c162d719
BorovChe <120989445+BorovChe@users.noreply.github.com>
2025-02-13 00:58:48 +0200

    add image picker and share meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,40 @@
+import { Suspense } from "react";
+import Link from "next/link";
+
+import { getMeals } from "@/lib/meals";
+import MealsGrid from "@/components/meals/meals-grid";
+
+import classes from "./page.module.css";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
 const MealsPage = () => {
-  return <h1 style={{ textAlign: "center", color: "white" }}>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favotite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Loading meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
